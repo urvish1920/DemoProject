f51266802ef909512cca4cb7617520bae3256fc0
urvish patel <urvishpatel192011@gmail.com>
2024-02-21 22:43:34 +0530

    done upto smtp

--- src/components/ComposeMail.jsx
+++ src/components/ComposeMail.jsx
@@ -1,42 +1,110 @@
-import React from 'react'
-import { Box, Dialog, InputBase, styled } from '@mui/material'
-import { Close } from '@mui/icons-material'
+import React from "react";
+import {
+  Box,
+  Button,
+  Dialog,
+  InputBase,
+  styled,
+  TextField,
+} from "@mui/material";
+import { Close, DeleteOutline } from "@mui/icons-material";
 
 const dialogstyle = {
-    height: '90%',
-    width: '80%',
-    maxWidth: '100%',
-    maxHeight: '100%',
-    boxShadow: 'none',
-    borderRadius: '10px 10px 0 0'
-}
+  height: "90%",
+  width: "80%",
+  maxWidth: "100%",
+  maxHeight: "100%",
+  boxShadow: "none",
+  borderRadius: "10px 10px 0 0",
+};
 const Headers = styled(Box)({
-    display: 'flex',
-    justifyContent: 'space-between',
-    background: '#f2f6fc',
-    '& > p': {
-        fontSize: 14,
-        fontWeight: 500,
-        padding: '0px 15px',
-    },
-    '& > svg': {
-        padding: '10px 15px'
-    }
-})
+  display: "flex",
+  justifyContent: "space-between",
+  background: "#f2f6fc",
+  "& > p": {
+    fontSize: 14,
+    fontWeight: 500,
+    padding: "0px 15px",
+  },
+  "& > svg": {
+    padding: "12px 15px",
+  },
+});
 
-const ComposeMail = () => {
-    return (
-        <div>
-            <Dialog open={true} PaperProps={{ sx: dialogstyle }}>
-                <Headers><p>New Message</p><Close fontSize='small' /></Headers>
-                <Box>
-                    <InputBase></InputBase>
-                </Box>
-                <Box>textarea</Box>
-                <Box></Box>
-            </Dialog>
-        </div>
-    )
-}
+const ReceipientsWrapperr = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  padding: "0 15px",
+  "& > div": {
+    fontSize: 14,
+    borderBottom: "1px solid #F5F5F5",
+    marginTop: 10,
+  },
+});
 
-export default ComposeMail
+const Footer = styled(Box)({
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  right: 0,
+  display: "flex",
+  justifyContent: "space-between",
+  padding: "10px 15px",
+  alignItems: "center",
+});
+
+const SendButton = styled(Button)({
+  background: "#0B57D0",
+  color: "#fff",
+  borderRadius: 20,
+  fontWeight: 500,
+  textTransform: "none",
+  width: 100,
+});
+
+const ComposeMail = ({ openDialog, setOpenDialog }) => {
+  const closeComponentMail = (e) => {
+    e.preventDefault();
+    setOpenDialog(false);
+  };
+
+  const sendMail = () => {
+    setOpenDialog(false);
+  };
+  return (
+    <div>
+      <Dialog open={openDialog} PaperProps={{ sx: dialogstyle }}>
+        <Headers>
+          <p>New Message</p>
+          <Close
+            fontSize="small"
+            onClick={(e) => {
+              closeComponentMail(e);
+            }}
+          />
+        </Headers>
+        <ReceipientsWrapperr>
+          <InputBase placeholder="Receipients" />
+          <InputBase placeholder="Subject" />
+        </ReceipientsWrapperr>
+        <TextField
+          multiline
+          rows={20}
+          sx={{ "& .MuiOutlinedInput-notchedOutline": { border: "none" } }}
+        />
+        <Footer>
+          <SendButton
+            onClick={() => {
+              sendMail();
+            }}
+          >
+            Send
+          </SendButton>
+          <DeleteOutline onClick={() => setOpenDialog(false)} />
+        </Footer>
+      </Dialog>
+    </div>
+  );
+};
+
+export default ComposeMail;
--- src/components/SidebarContent.jsx
+++ src/components/SidebarContent.jsx
@@ -1,48 +1,55 @@
-import React from 'react'
-import { Box, Button, styled, List, ListItem } from '@mui/material'
-import { CreateOutlined } from '@mui/icons-material'
-import { SIDEBAR_DATA } from '../config/sidebar.config'
-import ComposeMail from './ComposeMail'
+import React, { useState } from "react";
+import { Box, Button, styled, List, ListItem } from "@mui/material";
+import { CreateOutlined } from "@mui/icons-material";
+import { SIDEBAR_DATA } from "../config/sidebar.config";
+import ComposeMail from "./ComposeMail";
 
 const ComposeButton = styled(Button)({
-    background: '#c2e7ff',
-    color: '#001d35',
-    padding: 16,
-    borderRadius: 16,
-    minWidth: 140,
-    textTransform: 'none',
-})
+  background: "#c2e7ff",
+  color: "#001d35",
+  padding: 16,
+  borderRadius: 16,
+  minWidth: 140,
+  textTransform: "none",
+});
 const Container = styled(Box)({
-    padding: 8,
-    '& > ul': {
-        padding: '10px 0 0 5px',
-        fontSize: 14,
-        fontWeight: 500,
-        cursor: "pointer",
-    },
-    '& > ul > li > svg': {
-        marginRight: 20
-    }
-
-})
+  padding: 8,
+  "& > ul": {
+    padding: "10px 0 0 5px",
+    fontSize: 14,
+    fontWeight: 500,
+    cursor: "pointer",
+  },
+  "& > ul > li > svg": {
+    marginRight: 20,
+  },
+});
 const SidebarContent = () => {
-    return (
-        <Container>
-            <ComposeButton style={{ marginTop: 15, marginRight: 80 }}>
-                <CreateOutlined /> Compose
-            </ComposeButton>
-            <List style={{ marginLeft: 10 }}>
-                {
-                    SIDEBAR_DATA.map(data => (
-                        <ListItem>
-                            <data.icon fontSize='small' />{data.title}
-                        </ListItem>
-                    ))
-                }
-            </List>
-            <ComposeMail />
-        </Container >
-    )
-}
+  const [openDialog, setOpenDialog] = useState();
+  const onComposeClick = () => {
+    setOpenDialog(true);
+  };
+  return (
+    <Container>
+      <ComposeButton
+        style={{ marginTop: 15, marginRight: 80 }}
+        onClick={() => {
+          onComposeClick();
+        }}
+      >
+        <CreateOutlined /> Compose
+      </ComposeButton>
+      <List style={{ marginLeft: 10 }}>
+        {SIDEBAR_DATA.map((data) => (
+          <ListItem>
+            <data.icon fontSize="small" />
+            {data.title}
+          </ListItem>
+        ))}
+      </List>
+      <ComposeMail openDialog={openDialog} setOpenDialog={setOpenDialog} />
+    </Container>
+  );
+};
 
-export default SidebarContent
+export default SidebarContent;
